da058de792cbea395c3b7f28613ed528ce8f3837
$zzz☠ <126430210+666snake999@users.noreply.github.com>
2023-11-23 02:46:26 -0300

    Update consulta_cpf.py

--- consulta_cpf.py
+++ consulta_cpf.py
@@ -1,90 +1,65 @@
 import requests
 import pprint
-print('''███████╗███╗ ██╗ █████╗ ██╗ ██╗███████╗
+import json 
+
+Mblack = '\033[1;30m'   # Preto
+Ired = '\033[1;31m'     # Vermelho
+Dgreen = '\033[1;32m'   # Verde
+Nyellow = '\033[1;33m'  # Amarelo
+Iblue = '\033[1;34m'    # Azul
+Gpurple = '\033[1;35m'  # Roxo
+Hcyan = '\033[1;36m'    # Ciano
+Twhite = '\033[1;37m'   # Branco
+VRCRM = '\033[0;0m'     # Remover
+
+print(f'''{Gpurple}
+███████╗███╗ ██╗ █████╗ ██╗ ██╗███████╗
 ██╔════╝████╗ ██║██╔══██╗██║ ██╔╝██╔════╝
 ███████╗██╔██╗ ██║███████║█████╔╝ █████╗  
 ╚════██║██║╚██╗██║██╔══██║██╔═██╗ ██╔══╝  
 ███████║██║ ╚████║██║ ██║██║ ██╗███████╗
 ╚══════╝╚═╝ ╚═══╝╚═╝ ╚═╝╚═╝ ╚═╝╚══════╝
-''')
+{VRCRM}''')
+
+def consultar_nome(nome):
+    url = f'https://apisdedicado.nexos.dev/SerasaNome/nome?token=2ae274ad75c45b657547631a82358dbc&nome={nome}'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        pprint.pprint(data)
+    else:
+        print(f"{Ired}Erro na consulta do nome.{VRCRM}")
+
 def consultar_cpf(cpf):
     url = f'https://apisdedicado.nexos.dev/SerasaCpf/cpf?token=2ae274ad75c45b657547631a82358dbc&cpf={cpf}'
+    response = requests.get(url)    
+    if response.status_code == 200:
+        data = response.json()
+        pprint.pprint(data)
+    else:
+                print(f"{Ired}Erro na consulta do cpf.{VRCRM}")
 
-    try:
-        response = requests.get(url)
+def exibir_menu():
+    print(f"{Hcyan}Menu:")
+    print("1. Consultar nome")
+    print("2. Consultar cpf")
+    print("3. Sair")
 
-        if response.status_code == 200:
-            resultado = response.json()
-            return resultado
+def menu():
+    while True:
+        exibir_menu()
+        opcao = input("Digite a opção desejada: ")
+
+        if opcao == "1":
+            nome = input('Digite o nome a ser consultado: ')
+            consultar_nome(nome)
+        elif opcao == "2":
+            cpf = input('Digite o cpf a ser consultado: ')
+            consultar_cpf(cpf)
+        elif opcao == "3":
+            print(f"{Nyellow}Encerrando o programa...{VRCRM}")
+            break 
         else:
-            print('Erro na requisição. Código de status:', response.status_code)
-            return None
+            print(f"{Ired}Opção inválida. Tente novamente.{VRCRM}")
 
-    except requests.exceptions.RequestException as e:
-        print('Erro na requisição:', str(e))
-        return None
-
-# Função para substituir valores vazios por "Sem informação" e adicionar o DDD
-def substituir_valor_vazio(valor):
-    if not valor:
-        return "Sem informação"
-    return valor
-
-# Função para tratar formatos diferentes de data
-def formatar_data(data):
-    if data and len(data) >= 10:
-        return data[:10]
-    return "Sem informação"
-
-# Solicita ao usuário que digite o CPF
-cpf = input("Digite o CPF a ser consultado: ")
-
-# Chama a função para consultar o CPF
-resultado = consultar_cpf(cpf)
-
-# Exibe os resultados no formato desejado
-if resultado:
-    pp = pprint.PrettyPrinter(indent=4)
-    dados_formatados = {
-        "NOME": substituir_valor_vazio(resultado['results']['NOME']),
-        "DATA DE NASCIMENTO": formatar_data(resultado['results']['NASC']),
-        "NOME DA MÃE": substituir_valor_vazio(resultado['results']['NOME_MAE']),
-        "NOME DO PAI": substituir_valor_vazio(resultado['results']['NOME_PAI']),
-        "SEXO": substituir_valor_vazio(resultado['results']['SEXO'])
-    }
-
-    emails = resultado['results'].get('emails')
-    if emails:
-        dados_formatados["EMAIL"] = [
-            substituir_valor_vazio(email.get('EMAIL'))
-            for email in emails
-        ]
-
-    telefones = resultado['results'].get('telefones')
-    if telefones:
-        dados_formatados["TELEFONE"] = [
-            f"({telefone.get('DDD')}) {substituir_valor_vazio(telefone.get('TELEFONE'))}"
-            for telefone in telefones
-        ]
-
-    enderecos = resultado['results'].get('enderecos')
-    if enderecos:
-        dados_formatados["ENDEREÇOS"] = [
-            {
-                "ENDEREÇO": substituir_valor_vazio(endereco.get('LOGR_NOME')),
-                "BAIRRO": substituir_valor_vazio(endereco.get('BAIRRO')),
-                "CIDADE": substituir_valor_vazio(endereco.get('CIDADE')),
-                "UF": substituir_valor_vazio(endereco.get('UF')),
-                "CEP": substituir_valor_vazio(endereco.get('CEP'))
-            }
-            for endereco in enderecos
-        ]
-
-    dados_formatados["ORGÃO EMISSOR"] = substituir_valor_vazio(resultado['results']['ORGAO_EMISSOR'])
-    dados_formatados["TITULO DE ELEITOR"] = substituir_valor_vazio(resultado['results']['TITULO_ELEITOR'])
-    dados_formatados["NACIONALIDADE"] = substituir_valor_vazio(resultado['results']['NACIONALID'])
-    dados_formatados["RENDA"] = substituir_valor_vazio(resultado['results']['RENDA'])
-
-    pp.pprint(dados_formatados)
-else:
-    print('Não foi possível obter os dados do CPF', cpf)
+menu()
